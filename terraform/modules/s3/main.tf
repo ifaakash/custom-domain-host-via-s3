e87196f402068e7f0f9be23abf416ec37aafa33d
ifaakash <aakashc.appregis@gmail.com>
2025-07-17 04:37:12 +0530

    chore(website): Add error page for the website

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -8,6 +8,16 @@ resource "aws_s3_bucket" "s3" {
   }
 }
 
+resource "aws_s3_bucket_website_configuration" "hosting" {
+  bucket = aws_s3_bucket.s3.id
+  index_document {
+    suffix = "landing.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
 
 output "bucket-name" {
   description = "Name of the S3 bucket created"
