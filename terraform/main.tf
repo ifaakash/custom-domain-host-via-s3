e0c9414e0e6991288d745870763d1ab9e1d781ce
ifaakash <aakashc.appregis@gmail.com>
2025-07-17 03:34:30 +0530

    chore(IAM: Using assume role and provider alias for terraform execution

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,30 @@
 module "s3" {
   source = "./modules/s3"
 }
+
+
+module "iam" {
+  source      = "./modules/iam"
+  bucket_name = module.s3.bucket-name
+}
+
+
+provider "aws" {
+  alias  = "lpp"
+  region = "us-east-2"
+  assume_role {
+    role_arn = "arn:aws:iam::217471730172:role/terraform-execution-role"
+  }
+}
+
+
+output "execution_role_arn" {
+  description = "ARN for the role that allow action via terraform"
+  value       = module.iam.execution_role_arn
+}
+
+
+output "bucket-name" {
+  description = "Name of the S3 bucket created"
+  value       = module.s3.bucket-name
+}
--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -4,10 +4,32 @@
 #   policy = aws_iam_policy.allow-s3.
 # }
 
-resource "aws_iam_policy" "allow-s3" {
+resource "aws_iam_role" "terraform_execution_role" {
+  name = "terraform-execution-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::217471730172:user/admin"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+output "execution_role_arn" {
+  description = "ARN for the role that allow action via terraform"
+  value       = aws_iam_role.terraform_execution_role.arn
+}
+
+resource "aws_iam_policy" "allow_s3" {
+  provider    = aws.root
   name        = "allow-s3-access"
   description = "Policy to allow access to S3 bucket, for reading and writing"
-  path        = "/poc"
+  path        = "/"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -21,7 +43,20 @@ resource "aws_iam_policy" "allow-s3" {
   })
 }
 
+# data "aws_caller_identity" "current" {}
+
+# resource "aws_iam_policy_attachment" "allow_get_put_over_s3" {
+#   name       = "allow-get-put-over-s3"
+#   policy_arn = aws_iam_policy.allow_s3.arn
+#   users      = data.aws_caller_identity.current.user_id
+# }
+
 variable "bucket_name" {
   description = "Name of S3 bucket in discussion"
   type        = string
+}
+
+provider "aws" {
+  alias  = "root"
+  region = "us-east-2"
 }
